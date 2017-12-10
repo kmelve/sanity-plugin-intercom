1423d88ff2ca99f9a835ca83299c2d76615174e1
Knut Melvær <knut.melvaer@netlife.com>
2017-12-10 21:34:44 +0100

    add babel stuff

--- src/IntercomIcon.js
+++ src/IntercomIcon.js
@@ -1,14 +1,6 @@
 import React from 'react';
 
-/**
- * Couple of things to note:
- * - width and height is set to 1em
- * - fill is `currentColor` - this will ensure that the icon looks uniform and
- *   that the hover/active state works. You can of course render anything you
- *   would like here, but for plugins that are to be used in more than one
- *   studio, we suggest these rules are followed
- **/
-export default () => (
+const Icon = () => (
   <svg
     width="1em"
     height="1em"
@@ -22,3 +14,5 @@ export default () => (
     />
   </svg>
 );
+
+export default Icon;
