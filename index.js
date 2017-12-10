0082f472fb11f269c43cd957eb8e9d06c80a2052
Knut Melvær <knut.melvaer@netlife.com>
2017-12-10 21:06:32 +0100

    tie the bow

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import IntercomTool from './IntercomTool';
+import IntercomIcon from './IntercomIcon';
+
+export default {
+  title: 'Intercom',
+  name: 'intercom',
+  icon: IntercomIcon,
+  component: IntercomTool
+};
