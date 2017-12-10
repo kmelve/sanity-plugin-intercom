117a344e65f355e5ed3e1d3b6776dd31a16a9c28
Knut Melvær <knut.melvaer@netlife.com>
2017-12-10 21:05:37 +0100

    add plugin

---
+++ IntercomTool.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Intercom from 'react-intercom';
+import config from 'config:@sanity/intercom';
+import styles from './IntercomTool.css';
+
+class IntercomTool extends Component {
+  render() {
+    const {
+      appId = false,
+      email = 'No email set',
+      name = 'No name set'
+    } = config;
+    if (!appId)
+      return (
+        <div className={styles.tool}>
+          You need to set an Intercom Id in the plugin settings in
+          config/intercom
+        </div>
+      );
+    return (
+      <div className={styles.tool}>
+        <p>Use the Intercom widget in the bottom right corner to contact us.</p>
+        <ul>
+          Settings in config/intercom.js
+          <li>appId: {appId}</li>
+          <li>appId: {name}</li>
+          <li>appId: {email}</li>
+        </ul>
+        <Intercom appID={appId} />
+      </div>
+    );
+  }
+}
+
+export default IntercomTool;
